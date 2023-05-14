97eca55e3de163effe3f1c5ba95963f3c276b95f
NairClarra <nairclarra@gmail.com>
2023-05-14 01:19:09 -0300

    Initial commit

---
+++ Examples/C#/Managed NDI Recv/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using NewTek;
+using NewTek.NDI;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Accord.Video.FFMPEG;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+using DotNetPusher.Pushers;
+using System.Runtime.InteropServices;
+
+namespace Managed_NDI_Receive
+{
+
+    
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int index);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt([In] IntPtr hdc, int nXDest, int nYDest, int nWidth, int nHeight, [In] IntPtr hdcSrc, int nXSrc, int nYSrc, uint dwRop);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hdc);
+
+        public int recStart = 0;
+        public int serverStarted = 0;
+        public WebSocketServer wssv = new WebSocketServer("ws://localhost:8050");
+  
+        public string byteToString(byte[] buffer)
+        {
+            string message;
+            int endIndex;
+            message = Encoding.ASCII.GetString(buffer);
+            endIndex = message.IndexOf('\0');
+            if (endIndex > 0) message = message.Substring(0, endIndex);
+            return message;
+        }
+        public DotNetPusher.Encoders.Encoder encoder = new DotNetPusher.Encoders.Encoder(1920,1080,25, 1024*800);
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+          
+
+            // Not required, but "correct". (see the SDK documentation)
+            if (!NDIlib.initialize())
+            {
+                // Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
+                // you can check this directly with a call to NDIlib.is_supported_CPU()
+                if (!NDIlib.is_supported_CPU())
+                {
+                    MessageBox.Show("CPU unsupported.");
+                }
+                else
+                {
+                    // not sure why, but it's not going to run
+                    MessageBox.Show("Cannot run NDI.");
+                }
+
+                // we can't go on
+                Close();
+            }
+            
+            
+        }
+
+        // properly dispose of the unmanaged objects
+        protected override void OnClosed(EventArgs e)
+        {
+            if (ReceiveViewer != null)
+                ReceiveViewer.Dispose();
+
+            if (_findInstance != null)
+                _findInstance.Dispose();
+
+            base.OnClosed(e);
+        }
+        private WriteableBitmap VideoBitmap;
+        // This will find NDI sources on the network.
+        // Continually updated as new sources arrive.
+        // Note that this example does see local sources (new Finder(true))
+        // This is for ease of testing, but normally is not needed in released products.
+        public Finder FindInstance
+        {
+            get { return _findInstance; }
+        }
+        private Finder _findInstance = new Finder(true);
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewTek.NDI.WPF.ReceiveView.recStart = 1;
+
+            
+        }
+
+        private void SourcesSelector_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Server_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (serverStarted == 0)
+            {
+                wssv.AddWebSocketService<Echo>("/Echo");
+                wssv.Start();
+                serverStarted = 1;
+            }
+            else
+            {
+                wssv.Stop();
+                serverStarted = 0;
+            }
+
+           
+
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewTek.NDI.WPF.ReceiveView.recStart = 2;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewTek.NDI.WPF.ReceiveView.recStart = 0;
+        }
+    }
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.Data == "RECORD") NewTek.NDI.WPF.ReceiveView.recStart = 1;
+            else if ( e.Data.Contains("FILENAME") )
+            {                NewTek.NDI.WPF.ReceiveView.fileName = e.Data.Split('|')[1];
+            }
+            else if (e.Data == "STOP") NewTek.NDI.WPF.ReceiveView.recStart = 0;
+            else if(e.Data=="PAUSE") NewTek.NDI.WPF.ReceiveView.recStart = 2;
+
+        }
+    }
+}
---
+++ Examples/C#/Managed NDI Recv/Recorder.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Accord.Video.FFMPEG;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Managed_NDI_Receive
+{
+    internal class Recorder
+    {
+        //video variables
+        private Rectangle bounds;
+        private string outputPath = "";
+        private string tempPath = "";
+        private int fileCount = 1;
+        private List<string> inputImageSequence = new List<string>();
+
+        //file variables
+        private string audioName = "mic.wav";
+        private string videoName = "video.wav";
+        private string finalNAme = "finalVideo.mp4";
+
+
+        //time variable
+        Stopwatch watch = new Stopwatch();
+
+        //audio variables
+        public static class NativeMethods
+        {
+            [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+            public static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        }
+
+        public void ScreenRecorder(Rectangle b, string outPath)
+        {
+            CreateTempFolder("tempScreenshots");
+
+            bounds = b;
+            outputPath = outPath;
+            
+
+        }
+        private void CreateTempFolder(string name) {
+            if (Directory.Exists("D://"))
+            {
+                string pathName = $"D://{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+            else
+            {
+                string pathName = $"C://{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+        }
+        private void DeletePath(string targetDir)
+        {
+            string[] files = Directory.GetFiles(targetDir);
+            string[] dirs = Directory.GetDirectories(targetDir);
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+            foreach (string dir in dirs)
+            {
+                DeletePath(dir);
+            }
+            Directory.Delete(targetDir, false);
+            
+        }
+        private void DeleteFilesExcept(string targetFile, string excFile)
+        {
+            string [] files= Directory.GetFiles(targetFile); 
+            foreach(string file in files)
+            {
+                if (file != excFile)
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+            }
+        }
+        public void cleanup()
+        {
+            if (Directory.Exists(tempPath))
+            {
+                DeletePath(tempPath);
+            }
+        }
+        public string getElapsed()
+        {
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", watch.Elapsed.Hours, watch.Elapsed.Minutes, watch.Elapsed.Seconds);
+
+        }
+        public void RecordVideo()
+        {
+            watch.Start();
+
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap)){
+                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+
+                }
+                string name = tempPath = "//screenshot-" + fileCount + ".png";
+                bitmap.Save(name, ImageFormat.Png);
+                inputImageSequence.Add(name);
+                fileCount++;
+
+                bitmap.Dispose();
+
+            }
+            
+        }
+    }   
+
+}
